506d21e1ff790fd0e35ac1059ea4a8bc462599d0
khoakim <khoakim@atosp.onmicrosoft.com>
2021-09-21 15:28:08 +1000

    test

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import IndexScreen from './src/IndexScreen';
+
+const navigator = createStackNavigator({
+  Home:IndexScreen
+},{
+  initialRouteName:'Home',
+  defaultNavigationOptions: {
+    title:'Home'
+  }
+})
+const App =  createAppContainer(navigator);
+
+export default () => {
+  return <App />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/IndexScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+
+const IndexScreen = () => {
+    return (
+        <View>
+            <Text>asdfsssx</Text>
+        </View>
+    )
+}
+
+export default IndexScreen;
