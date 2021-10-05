46d5f8b72d37bee9a64acc6ab4bbcf3f6cb9f211
Khoa Kim <khoa.kim85@gmail.com>
2021-10-05 17:39:41 +1100

    using forms

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,43 +1,14 @@
 import React, {useContext, useState} from 'react';
-import {View,Text,StyleSheet, TextInput, Button} from 'react-native';
 import { Context as BlogContext} from '../context/BlogContext';
-
+import BlogPostForm from '../component/BlogPostForm';
 const CreateScreen = ({navigation}) => {
-    const [title,setTitle] = useState("");
-    const [content,setContent]=useState('');
-    const {state,addBlogPost}=useContext(BlogContext);
-    return (
-        <View>
-            <Text style={styles.label}>Enter Title:</Text>
-            <TextInput 
-                style={styles.input}
-                value={title} 
-                onChangeText={(text)=>setTitle(text)}/>
-            <Text style={styles.label}>Enter Content:</Text>
-            <TextInput 
-                style={styles.input}
-                value={content} 
-                onChangeText={(text)=>setContent(text)}/>
-            <Button title="Add Blog Post"
-                onPress={()=>
-                    addBlogPost(title,content, () => navigation.navigate('Home'))
-                    }/>
-        </View>
-    )
-};
 
-const styles = StyleSheet.create({
-    input : {
-        fontSize : 18,
-        borderWidth : 1,
-        borderColor : 'black',
-        marginBottom : 15,
-        marginHorizontal : 5
-    },
-    label : {
-        fontSize : 20,
-        marginBottom : 5,
-    }
-});
+    const {addBlogPost}=useContext(BlogContext);
+    return <BlogPostForm 
+                onSubmit={(title,content)=>{
+                    addBlogPost(title,content, () => navigation.navigate('Home'))
+                }}
+                />
+};
 
 export default CreateScreen;
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,50 +1,15 @@
-import React, {useState,useContext} from "react";
-import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+import React, {useContext} from "react";
 import { Context as BlogContext } from "../context/BlogContext";
+import BlogPostForm from "../component/BlogPostForm";
 const EditScreen = ({navigation}) => {
     const {state,updateBlogPost} = useContext(BlogContext);
     const blogPost = state.find((blogPost)=>blogPost.id===navigation.getParam('id'));
-    console.log(navigation.getParam('id'));
-
-    const [title,setTitle] = useState(blogPost.title);
-    const [content,setContent]=useState(blogPost.content);
-    
-    return (
-                
-            <View>      
-            <Text style={styles.label}>Enter Title:</Text>
-            <TextInput 
-                style={styles.input}
-                value={title}
-                onChangeText={(text)=>setTitle(text)}/>
-            <Text style={styles.label}>Enter Content:</Text>
-            <TextInput 
-                style={styles.input}
-                value={content}
-                onChangeText={(text)=>setContent(text)}/>
-            <Button title="Update"
-                onPress={()=>
+    return <BlogPostForm 
+                initialValues={{title:blogPost.title,content:blogPost.content}}
+                onSubmit={(title,content)=>{
                     updateBlogPost(navigation.getParam('id'),title,content, () => navigation.navigate('Home'))
-                    }/>
-        </View>
-        
-
-        
-    )
+            }}
+    /> 
 };
-
-const styles = StyleSheet.create({
-    input : {
-        fontSize : 18,
-        borderWidth : 1,
-        borderColor : 'black',
-        marginBottom : 15,
-        marginHorizontal : 5
-    },
-    label : {
-        fontSize : 20,
-        marginBottom : 5,
-    }
-});
 
 export default EditScreen;
